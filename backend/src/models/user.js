ee4105c2b02dc6fbffe36106e639151049b5e5f7
sahan <edu.sahan@gmail.com>
2026-01-01 17:14:15 +0530

    Implemented user creation with email and password hashing using bcrypt.

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -2,11 +2,17 @@ const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema(
   {
-    googleId: String,
-    name: String,
-    email: { type: String, unique: true, required: true },
-    avatar: String,
+    email: {
+      type: String,
+      required: true
+    },
+
+    password: {
+      type: String,
+      required: true
+    }
   },
+
   { timestamps: true }
 );
 
